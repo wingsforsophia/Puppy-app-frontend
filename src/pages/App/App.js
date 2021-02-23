212be816bb0b82a66e2d3e2eaa00ac9af1b2c022
Sophia Seremetis <shseremetis@gmail.com>
2021-02-23 12:30:29 -0500

    add nav links

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, NavLink} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    puppies: []
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          React Puppies CRUD
+          <nav>
+            <NavLink exact to='/'>PUPPIES LIST</NavLink>
+            &nbsp;&nbsp;&nbsp;
+            <NavLink exact to='/add'>ADD PUPPY</NavLink>
+          </nav>
+        </header>
+        <main>
+          
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
